6a71b3180d171a0935c5d252aab88e964ce4c291
Cameron Little <cameron@camlittle.com>
2020-10-24 09:59:16 +0200

    Hoist bound global console

--- src/dependencyManagement.ts
+++ src/dependencyManagement.ts
@@ -28,26 +28,26 @@ interface InstallationOptions {
   console?: Partial<Console> | null;
 }
 
+const boundGlobalConsole: typeof console = {
+  assert: console.assert.bind(console),
+  clear: console.clear.bind(console),
+  log: console.log.bind(console),
+  info: console.info.bind(console),
+  warn: console.warn.bind(console),
+  error: console.error.bind(console),
+  group: console.group.bind(console),
+  groupEnd: console.groupEnd.bind(console),
+  count: console.count.bind(console),
+  time: console.time.bind(console),
+  timeEnd: console.timeEnd.bind(console),
+  timeStamp: console.timeStamp?.bind(console),
+  trace: console.trace.bind(console),
+};
+
 export async function installWrappedDependencies(
   compositeDisposable: CompositeDisposable,
   options: InstallationOptions = {}
 ) {
-  const boundGlobalConsole: typeof console = {
-    assert: console.assert.bind(console),
-    clear: console.clear.bind(console),
-    log: console.log.bind(console),
-    info: console.info.bind(console),
-    warn: console.warn.bind(console),
-    error: console.error.bind(console),
-    group: console.group.bind(console),
-    groupEnd: console.groupEnd.bind(console),
-    count: console.count.bind(console),
-    time: console.time.bind(console),
-    timeEnd: console.timeEnd.bind(console),
-    timeStamp: console.timeStamp?.bind(console),
-    trace: console.trace.bind(console),
-  };
-
   const logger =
     options.console === null
       ? null
